6cf3853d0c7249b7e789cf2e480ca9829855a167
mstavro <martin.stavro@spartans.ut.edu>
2022-10-18 15:03:00 -0400

    Update DPMIR Repression New PM Robustness Test.R

--- Robustness Tests/Repression New PM Robustness Check/DPMIR Repression New PM Robustness Test.R
+++ Robustness Tests/Repression New PM Robustness Check/DPMIR Repression New PM Robustness Test.R
@@ -22,7 +22,7 @@ glm5 <- glm(newpolice ~ polity2_P4 + repress_index_lagged + gdp_WDI_log10 + came
               lji_LS, data = data, family = "binomial")
 glm6 <- glm(newpolice ~ repress_index + polity2_P4 + gdp_WDI_log10 + cameo_protests + hasNHRI + pop_WDI_log10 + 
               lji_LS, data = data, family = "binomial")
-colabs2 <- c("Repression", "Number of protests", "Polity IV score", "Latent judicial independence", "NHRI presence", "Log GDP (constant 2010 USD)", "Log population", "Repression t-1")
-output2 <- stargazer(glm1, glm6, glm3, glm2, glm4, glm5, order = c(1,5,2,8,6,4,7,3), covariate.labels = colabs2, dep.var.labels = "Newly Militarized Police", type = "html")
+colabs2 <- c("Repression", "Repression t-1", "Number of protests", "Polity IV score", "Latent judicial independence", "NHRI presence", "Log GDP (constant 2010 USD)", "Log population")
+output2 <- stargazer(glm1, glm6, glm3, glm2, glm4, glm5, order = c(1,3,5,2,8,6,4,7), covariate.labels = colabs2, dep.var.labels = "Newly Militarized Police", type = "html")
 output2 <- str_replace(output2, "Repression t-1", "Repression<sub> t-1</sub>")
 write_lines(output2, "DPMIR_NewMilitariz_EndoTest.html")
